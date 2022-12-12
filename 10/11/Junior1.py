b00c45a96ce78682b16a5c5c5e092b986e9fb47e
Rishi <satyajitdatta@gmail.com>
2022-12-11 20:09:18 -0500

    Completed 2011/Junior1 "Which Alien?"

--- 10/11/Junior1.py
+++ 10/11/Junior1.py
@@ -1,3 +1,9 @@
-pass
+antennae = int(input())
+eyes = int(input())
 
-# TODO
+if antennae >= 3 and eyes <= 4:
+    print("TroyMartian")
+if antennae <= 6 and eyes >= 2:
+    print("VladSaturnian")
+if antennae <= 2 and eyes <= 3:
+    print("GraemeMercurian")
